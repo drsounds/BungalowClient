41c184d2596d1d0656fca00d1b39a2e4fc37857a
Alexander Forselius <drsounds@gmail.com>
2017-05-25 18:59:43 +0200

    T

---
+++ Bungalow/ViewModel/PlaylistViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Spotify.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bungalow.ViewModel
+{
+    public class PlaylistViewModel
+    {
+
+        public Playlist Playlist {get;set;}
+        public Trackset Tracks { get; set; }
+    }
+}
